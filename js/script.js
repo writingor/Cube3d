5cd2fc3ff58dd23a6fc15c0e3434e60e5fb08498
Igor <98356121+writingor@users.noreply.github.com>
2022-03-13 12:35:51 +0100

    Add btns

--- js/script.js
+++ js/script.js
@@ -2,6 +2,32 @@
     let rotateX = 0;
     let rotateY = 0;
 
+    function transformCube () {
+        document.querySelector(".cube").style.transform =
+        "rotateY(" + rotateY + "deg)" + "rotateX(" + rotateX + "deg)";
+    }
+
+    // CHANGE VARIABLES
+
+    // on BUTTONS
+    document.querySelector("#toLeft").onclick = function(){
+        rotateY -= 10;
+        transformCube();
+    }
+    document.querySelector("#toDown").onclick = function(){
+        rotateX += 10;
+        transformCube();
+    }
+    document.querySelector("#toRight").onclick = function(){
+        rotateY += 10;
+        transformCube();
+    }
+    document.querySelector("#toUp").onclick = function(){
+        rotateX -= 10;
+        transformCube();
+    }
+    
+    // on KYE DOWN
     document.onkeydown = function(e) {
         if (e.keyCode === 37) {
             rotateY -= 10;
@@ -12,7 +38,7 @@
         } else if (e.keyCode === 40) {
             rotateX -= 10;
         }
-        document.querySelector(".cube").style.transform =
-        "rotateY(" + rotateY + "deg)" + "rotateX(" + rotateX + "deg)";
+        transformCube();
     }
+
 })();

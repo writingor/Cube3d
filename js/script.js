05522d003c2586876ef71d40608ea08b0da72236
Igor <98356121+writingor@users.noreply.github.com>
2022-03-13 18:42:56 +0100

    Add swipe

--- js/script.js
+++ js/script.js
@@ -49,5 +49,48 @@
 
     // TOUCH
 
+    // Вешаем на прикосновение функцию handleTouchStart
+    document.addEventListener('touchstart', handleTouchStart, false);
+    // А на движение пальцем по экрану - handleTouchMove      
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;
+    var yDown = null;
+
+    function handleTouchStart(evt) {
+        xDown = evt.touches[0].clientX;
+        yDown = evt.touches[0].clientY;
+    };
+
+    function handleTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+        
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            if (xDiff > 0) {
+                rotateY -= 36;
+            } else {
+                rotateY += 36;
+            }
+        } else {
+            if (yDiff > 0) {
+                rotateX += 36;
+            } else {
+                rotateX -= 36;
+            }
+        }
+        transformCube();
+
+        xDown = null;
+        yDown = null;
+    };
+
 
 })();

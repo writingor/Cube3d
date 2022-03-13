0e1708521aa5f5664c7bffb26dd00ca8364be0f6
Igor <98356121+writingor@users.noreply.github.com>
2022-03-13 13:03:44 +0100

    test swipe

--- js/script.js
+++ js/script.js
@@ -2,33 +2,33 @@
     let rotateX = 0;
     let rotateY = 0;
 
-    function transformCube () {
+    function transformCube() {
         document.querySelector(".cube").style.transform =
-        "rotateY(" + rotateY + "deg)" + "rotateX(" + rotateX + "deg)";
+            "rotateY(" + rotateY + "deg)" + "rotateX(" + rotateX + "deg)";
     }
 
     // CHANGE VARIABLES
 
     // on BUTTONS
-    document.querySelector("#toLeft").onclick = function(){
+    document.querySelector("#toLeft").onclick = function () {
         rotateY -= 10;
         transformCube();
     }
-    document.querySelector("#toDown").onclick = function(){
+    document.querySelector("#toDown").onclick = function () {
         rotateX -= 10;
         transformCube();
     }
-    document.querySelector("#toRight").onclick = function(){
+    document.querySelector("#toRight").onclick = function () {
         rotateY += 10;
         transformCube();
     }
-    document.querySelector("#toUp").onclick = function(){
+    document.querySelector("#toUp").onclick = function () {
         rotateX += 10;
         transformCube();
     }
-    
+
     // on KYE DOWN
-    document.onkeydown = function(e) {
+    document.onkeydown = function (e) {
         if (e.keyCode === 37) {
             rotateY -= 10;
         } else if (e.keyCode === 38) {
@@ -40,45 +40,49 @@
         }
         transformCube();
     }
-    
 
-    var d = //Получаем расстояния от начальной до конечной точек по обеим осям
-    {
-   	 x: touchStart.x - touchPosition.x,
-   	 y: touchStart.y - touchPosition.y
-    };
-    
-    if(Math.abs(d.x) > Math.abs(d.y)) //Проверяем, движение по какой оси было длиннее
-    {
-   	 if(Math.abs(d.x) > sensitivity) //Проверяем, было ли движение достаточно длинным
-   	 {
-   		 if(d.x > 0) //Если значение больше нуля, значит пользователь двигал пальцем справа налево
-   		 {
-            rotateY -= 10;
-            transformCube();
-   		 }
-   		 else //Иначе он двигал им слева направо
-   		 {
-            rotateY += 10;
-            transformCube();
-   		 }
-   	 }
+
+    function checkSwipe () {
+        var d = //Получаем расстояния от начальной до конечной точек по обеим осям
+        {
+            x: touchStart.x - touchPosition.x,
+            y: touchStart.y - touchPosition.y
+        };
+
+        if (Math.abs(d.x) > Math.abs(d.y)) //Проверяем, движение по какой оси было длиннее
+        {
+            if (Math.abs(d.x) > sensitivity) //Проверяем, было ли движение достаточно длинным
+            {
+                if (d.x > 0) //Если значение больше нуля, значит пользователь двигал пальцем справа налево
+                {
+                    rotateY -= 10;
+                    transformCube();
+                }
+                else //Иначе он двигал им слева направо
+                {
+                    rotateY += 10;
+                    transformCube();
+                }
+            }
+        }
+        else //Аналогичные проверки для вертикальной оси
+        {
+            if (Math.abs(d.y) > sensitivity) {
+                if (d.y > 0) //Свайп вверх
+                {
+                    rotateX += 10;
+                    transformCube();
+                }
+                else //Свайп вниз
+                {
+                    rotateX -= 10;
+                    transformCube();
+                }
+            }
+        }
     }
-    else //Аналогичные проверки для вертикальной оси
-    {
-   	 if(Math.abs(d.y) > sensitivity)
-   	 {
-   		 if(d.y > 0) //Свайп вверх
-   		 {
-            rotateX += 10;
-            transformCube();
-   		 }
-   		 else //Свайп вниз
-   		 {
-            rotateX -= 10;
-            transformCube();
-   		 }
-   	 }
-    }
+
+
+    document.querySelector("body").addEventListener("touchend", checkSwipe, false);
 
 })();

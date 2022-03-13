86bccd103ce022cf35defcbdd11b9b40e7a9da79
Igor <98356121+writingor@users.noreply.github.com>
2022-03-13 18:33:39 +0100

    Fixed invalid links.

--- js/script.js
+++ js/script.js
@@ -11,19 +11,19 @@
 
     // on BUTTONS
     document.querySelector("#toLeft").onclick = function () {
-        rotateY -= 30;
+        rotateY -= 36;
         transformCube();
     }
     document.querySelector("#toDown").onclick = function () {
-        rotateX -= 30;
+        rotateX -= 36;
         transformCube();
     }
     document.querySelector("#toRight").onclick = function () {
-        rotateY += 30;
+        rotateY += 36;
         transformCube();
     }
     document.querySelector("#toUp").onclick = function () {
-        rotateX += 30;
+        rotateX += 36;
         transformCube();
     }
     document.querySelector("#toClear").onclick = function () {
@@ -36,13 +36,13 @@
     // on KYE DOWN
     document.onkeydown = function (e) {
         if (e.keyCode === 37) {
-            rotateY -= 10;
+            rotateY -= 36;
         } else if (e.keyCode === 38) {
-            rotateX += 10;
+            rotateX += 36;
         } else if (e.keyCode === 39) {
-            rotateY += 10;
+            rotateY += 36;
         } else if (e.keyCode === 40) {
-            rotateX -= 10;
+            rotateX -= 36;
         }
         transformCube();
     }
